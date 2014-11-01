48021904ada06d46edcc9e29d954c810a8bc595f
Naoki Morita <namorit@gmail.com>
2014-11-01 19:05:41 +0900

    Initial Commit

---
+++ UIBezierPathRoundedCornersSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIBezierPathRoundedCornersSample
+//
+//  Created by MORITA NAOKI on 2014/11/01.
+//  Copyright (c) 2014年 molabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
